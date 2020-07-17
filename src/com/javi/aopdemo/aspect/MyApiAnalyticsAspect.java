3c3a06f53a6b8def853ca6bd3e22473bcb9e24ff
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-17 17:00:50 +0200

    added @Order to Aspects so thta tehy follow an order by adding Order(1), Order(2), Order(3)

--- src/com/javi/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/com/javi/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.javi.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 	
 	@Before("com.javi.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")//added fully qualified class name
